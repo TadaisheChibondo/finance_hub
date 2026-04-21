ffae2144f97dd9ed2a7a05a81c1c6ebe2ecc2242
TadaisheChibondo <n02428418x@students.nust.ac.zw>
2026-04-21 10:39:39 +0200

    chore: initialized offline database and changed app name in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,16 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:finance_hub/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('App loads smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame using the NEW class name.
+    await tester.pumpWidget(const FinanceHubApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that our new placeholder text is on the screen.
+    expect(find.text('Database Initialized. UI Pending.'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
+    // Verify that the old counter app text is gone.
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
